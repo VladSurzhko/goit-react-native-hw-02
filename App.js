c32110e94a5274739719a403ec00be6a64dee742
VladSurzhko <surzhkovlad69@gmail.com>
2023-07-18 19:52:05 +0200

    upi

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, ImageBackground, Keyboard } from "react-native";
+import { Registration } from "./screens/RegistrationScreen";
+import { Login } from "./screens/LoginScreen";
+import { useFonts } from "expo-font";
+import { TouchableWithoutFeedback } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Alindos</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("./image/PhotoBG.jpg")}
+          resizeMode="cover"
+          style={styles.image}
+        >
+          <Registration />
+          {/* <Login/> */}
+        </ImageBackground>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
   },
 });
+
